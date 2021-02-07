45058a493f83ed3efa56df0656b44e280000e74f
fccJashCoda <personalrevolution2014@gmail.com>
2021-02-08 00:26:20 +0100

    update: budget.py base class done

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -5,26 +5,59 @@ class Category:
         self.ledger = []
         self.balance = 0
 
-    def deposit(self, amount, description):
+    def deposit(self, amount, description=str()):
         self.balance += amount
-        payload = {
+        transaction = {
             "amount": amount,
             "description": description
         }
-        self.ledger.append(payload)
+        self.ledger.append(transaction)
 
-    def withdraw(self, amount, description):
-        pass
+    def withdraw(self, amount, description=str()):
+        if self.check_funds(amount):
+            self.balance -= amount
+            transaction = {
+                "amount": -amount,
+                "description": description
+            }
+            self.ledger.append(transaction)
+            return True
+        else:
+            return False
 
     def get_balance(self):
-        pass
+        return self.balance
 
     def transfer(self, amount, destination):
-        pass
+        if self.check_funds(amount):
+            withdraw_desc = f"Transfer to {destination.name}"
+            deposit_desc = f"Transfer from {self.name}"
+            self.withdraw(amount, withdraw_desc)
+            destination.deposit(amount, deposit_desc)
+            return True
+        return False
 
     def check_funds(self, amount):
-        pass
+        if self.balance - amount >= 0:
+            return True
+        return False
 
 
 def create_spend_chart():
     pass
+
+
+food = Category('Food')
+print(food.balance)
+print(food.ledger)
+food.deposit(1000, 'bitcoin')
+print(food.balance)
+print(food.ledger)
+food.withdraw(155, 'bitcoin')
+car = Category('Car')
+print(food.get_balance())
+food.transfer(100, car)
+print(food.get_balance())
+print(food.ledger)
+print(car.get_balance())
+print(car.ledger)
