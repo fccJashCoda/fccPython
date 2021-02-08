71b029af400347632cfef26c217aa2d54f39da5d
fccJashCoda <personalrevolution2014@gmail.com>
2021-02-09 00:40:49 +0100

    refactored create_spend_chart in budge.py

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -54,86 +54,72 @@ class Category:
 
 
 def create_spend_chart(arr):
-    # get the percentage spent by category rounded down to the nearest ten
 
-    total_spent = 0
-    spent_per_category = []
-    for category in arr:
-        amount_spent = abs(sum(x["amount"] if x["amount"] <
-                               0 else 0 for x in category.ledger))
-        print(amount_spent)
-        total_spent += amount_spent
-        spent_per_category.append(amount_spent)
-
-    percentage_spent = []
-    for x in spent_per_category:
-        percentage_spent.append(
-            ((x / total_spent) * 100) - (((x / total_spent) * 100) % 10))
-
-    # if total_spent > 0:
-    #     percentage = (amount_spent / total_spent) * 100
-    # else:
-    #     percentage = 0
-    # percentage_spent.append(int(percentage - (percentage % 10)))
-    # for category in arr:
-    #     amount_spent = abs(sum(x["amount"] if x["amount"] <
-    #                            0 else 0 for x in category.ledger))
-    #     gross_sum = sum(x["amount"] if x["amount"] >
-    #                     0 else 0 for x in category.ledger)
-
-    #     if gross_sum > 0:
-    #         percentage = (amount_spent / gross_sum) * 100
-    #     else:
-    #         percentage = 0
-    #     percentage_spent.append(int(percentage - (percentage % 10)))
-    print(percentage_spent)
-
-    # print out the bar chart
-    graph_lines = []
-    for row in range(100, -1, -10):
-        y_axis = (str(row) + '|').rjust(4, ' ')
-        for value in percentage_spent:
-            if value >= row:
-                if 'o' in y_axis:
-                    y_axis += 'o'.rjust(3, ' ')
-                else:
-                    y_axis += 'o'.rjust(2, ' ')
-            else:
-                y_axis += ' ' * 3
-        if 'o' in y_axis:
-            y_axis += '  \n'
-        else:
-            y_axis += ' \n'
-        graph_lines.append(y_axis)
-
-    # print the separator (should end 2 bars after the last name line)
-    separator = '-'.rjust(5, ' ') + '---' * len(arr) + '\n'
-    # print the category names
-    min_length = max([len(item.name) for item in arr])
-
-    name_lines = []
-    for n in range(min_length):
-        row = str()
+    def _calculate_percentages(arr):
+        total_spent = 0
+        spent_per_category = []
         for category in arr:
-            if not len(row):
-                if n < len(category.name):
-                    row += category.name[n].rjust(2, ' ')
-                else:
-                    row += ' ' * 2
-            else:
-                if n < len(category.name):
-                    row += category.name[n].rjust(3, ' ')
-                else:
-                    row += ' ' * 3
-        if n != min_length - 1:
-            row += '  \n'
-        else:
-            row += '  '
-        # row += '  \n'
-        row = row.rjust(len(row) + 4, ' ')
-        name_lines.append(row)
+            amount_spent = abs(sum(x["amount"] if x["amount"] <
+                                   0 else 0 for x in category.ledger))
+            total_spent += amount_spent
+            spent_per_category.append(amount_spent)
 
-    # print(min_length)
+        percentage_spent = []
+        for x in spent_per_category:
+            percentage_spent.append(
+                ((x / total_spent) * 100) - (((x / total_spent) * 100) % 10))
+
+        return percentage_spent
+
+    def _draw_graph(percentages):
+        graph_lines = []
+        for row in range(100, -1, -10):
+            y_axis = (str(row) + '|').rjust(4, ' ')
+            for value in percentages:
+                if value >= row:
+                    if 'o' in y_axis:
+                        y_axis += 'o'.rjust(3, ' ')
+                    else:
+                        y_axis += 'o'.rjust(2, ' ')
+                else:
+                    y_axis += ' ' * 3
+            if 'o' in y_axis:
+                y_axis += '  \n'
+            else:
+                y_axis += ' \n'
+            graph_lines.append(y_axis)
+
+        return graph_lines
+
+    def _draw_bottom_labels(arr):
+        min_length = max([len(item.name) for item in arr])
+        name_lines = []
+        for n in range(min_length):
+            row = str()
+            for category in arr:
+                if not len(row):
+                    if n < len(category.name):
+                        row += category.name[n].rjust(2, ' ')
+                    else:
+                        row += ' ' * 2
+                else:
+                    if n < len(category.name):
+                        row += category.name[n].rjust(3, ' ')
+                    else:
+                        row += ' ' * 3
+            if n != min_length - 1:
+                row += '  \n'
+            else:
+                row += '  '
+            row = row.rjust(len(row) + 4, ' ')
+            name_lines.append(row)
+        return name_lines
+
+    separator = '-'.rjust(5, ' ') + '---' * len(arr) + '\n'
+    percentages = _calculate_percentages(arr)
+    graph_lines = _draw_graph(percentages)
+    name_lines = _draw_bottom_labels(arr)
+
     multilign_graph = (f"Percentage spent by category\n"
                        f"{str().join(graph_lines)}"
                        f"{separator}"
